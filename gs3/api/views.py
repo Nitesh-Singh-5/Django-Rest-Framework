c4161dd35bbda389649d849328f849960b6b2436
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2020-11-22 15:14:50 +0530

    class based view for Crud Api

--- gs3/api/views.py
+++ gs3/api/views.py
@@ -5,10 +5,49 @@ from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import status
 
+# @api_view(['GET','POST','PUT','PATCH','DELETE'])
+# def student_api(request):
+#     if request.method == "GET":
+#         id = request.data.get('id')
+#         if id is not None:
+#             stu = Student.objects.get(id=id)
+#             serializer = StudentSerializer(stu)
+#             return Response(serializer.data)
+        
+#         stu = Student.objects.all()
+#         serializer = StudentSerializer(stu, many=True)
+#         return Response (serializer.data)
+
+#     if request.method == 'POST':
+#         serializer = StudentSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({'msg':'Data created'})
+            
+#         return Response(serializer.errors)
+
+#     if request.method == 'PUT':
+#         id = request.data.get('id')
+#         stu = Student.objects.get(pk=id)
+#         serializer = StudentSerializer(stu, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({"msg":"Data Updated"})
+#         return Response(serializer.errors)
+        
+#     if request.method == "DELETE":
+#         id = request.data.get("id")
+#         stu = Student.objects.get(pk=id)
+#         stu.delete()
+#         return Response({'msg':'Data Deleted'})
+
+
+# For this extra app not required bcs we are using browser DRF
+
 @api_view(['GET','POST','PUT','PATCH','DELETE'])
-def student_api(request):
+def student_api(request,pk=None):
     if request.method == "GET":
-        id = request.data.get('id')
+        id = pk
         if id is not None:
             stu = Student.objects.get(id=id)
             serializer = StudentSerializer(stu)
@@ -22,23 +61,30 @@ def student_api(request):
         serializer = StudentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'msg':'Data created'})
+            return Response({'msg':'Data created'},status=status.HTTP_201_CREATED)
             
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'PATCH':
+        id = id
+        stu = Student.objects.get(pk=id)
+        serializer = StudentSerializer(stu, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg":"Partial Data Updated"})
         return Response(serializer.errors)
 
     if request.method == 'PUT':
-        id = request.data.get('id')
+        id = id
         stu = Student.objects.get(pk=id)
-        serializer = StudentSerializer(stu, data=request.data, partial=True)
+        serializer = StudentSerializer(stu, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"msg":"Data Updated"})
+            return Response({"msg":"Complete Data Updated"})
         return Response(serializer.errors)
         
     if request.method == "DELETE":
-        id = request.data.get("id")
+        id = pk
         stu = Student.objects.get(pk=id)
         stu.delete()
-        return Response({'msg':'Data Deleted'})
-
-
+        return Response({'msg':'Data Deleted'},status=status.HTTP_200_OK)
