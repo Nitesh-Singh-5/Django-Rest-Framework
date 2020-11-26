4d353e78a6d45487326845869c101477a6f81dc5
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2020-11-26 15:01:52 +0530

    Session Authentication and Permission

---
+++ gs6/api/views.py
@@ -0,0 +1,16 @@
+# Session Authentication and Permission
+
+from django.shortcuts import render
+from .models import  Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+
