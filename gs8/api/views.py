e0a265792a8b2ca985c87a13da38967c3df083ec
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2020-12-08 18:06:33 +0530

    JSON web token

---
+++ gs8/api/views.py
@@ -0,0 +1,18 @@
+# Throttling
+
+from django.shortcuts import render
+from .models import  Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.throttling import AnonRateThrottle,UserRateThrottle
+from api.throtlling import JackRateThrottle
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    throttle_classes = [AnonRateThrottle, JackRateThrottle]
+
