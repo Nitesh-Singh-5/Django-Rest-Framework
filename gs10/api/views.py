b71f21d890ebc4ada0ed3cb52bb76ff172282ae6
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2020-12-09 22:37:01 +0530

    Filtering

---
+++ gs10/api/views.py
@@ -0,0 +1,13 @@
+# filtering
+from django.shortcuts import render
+from .models import  Student
+from .serializers import StudentSerializer
+from rest_framework.generics import ListAPIView
+
+# class StudentList(ListAPIView):
+#     # queryset = Student.objects.filter(passby='user1')
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+#     def get_queryset(self):
+#         user =self.request.user
+#         return Student.objects.filter(passby=user)
