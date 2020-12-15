6473de73beef6fabcb6e4fac7bde1f5dc080f24e
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2020-12-15 12:35:51 +0530

    Hyperlinked Serializer

---
+++ gs12/api/views.py
@@ -0,0 +1,10 @@
+# Hyperlinked Serializer
+
+from django.shortcuts import render
+from .serializers import StudentSerializer
+from .models import Student
+from rest_framework import viewsets
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
